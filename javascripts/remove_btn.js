8b465fa498e947943d6e9c7f9d749b10a82edf67
mir32 <mir32@example.com>
2020-02-29 18:55:40 +0900

    Automated commit at 2020-02-29 09:55:40 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/remove_btn.js
+++ javascripts/remove_btn.js
@@ -1,52 +1,58 @@
-mouseX = 0
-mousedown = false
-$('body').on("mousemove", (e)=> {
-  if(mousedown) {
-    d = mouseX - e.pageX
+tab_event = (down, move, up) => {
 
-    if($('.stock-body-panel').hasClass("open")) {
-      if(-60<=d && d<=0) {
-        $('.stock-body-panel').css({right:  d+60+'px'});
-      } else if (d<-60) {
-        $('.stock-body-panel').css({right:  0+'px'});
-      } else {
-        $('.stock-body-panel').css({right:  60+'px'});
-      }
-    } else {
-      if(0<=d && d<=60) {
-        $('.stock-body-panel').css({right:  d+'px'});
-      } else if(d>60) {
-        $('.stock-body-panel').css({right:  60+'px'});
-      } else {
-        $('.stock-body-panel').css({right:  0+'px'});
-      }
-
-    }
-  }
-})
-
-
-$('.stock-body-panel').mousedown(function(e){
-  mouseX = e.pageX
-  mousedown = true
-})
-
-$("body").mouseup(function(e){
-  if(mousedown) {
-  if($('.stock-body-panel').hasClass("open")) {
-
-      $('.stock-body-panel').animate({right: '0px'}, 30);
-      $('.stock-body-panel').removeClass("open")
-
-
-  } else {
-    if (mouseX - e.pageX < 20) {
-      $('.stock-body-panel').animate({right: '0px'}, 30);
-    } else {
-      $('.stock-body-panel').animate({right: '60px'}, 30);
-      $('.stock-body-panel').addClass("open")
-    }
-  }
+  mouseX = 0
   mousedown = false
-  }
-})
+
+  $('body').on(move, (e)=> {
+    if(mousedown) {
+      d = mouseX - e.pageX
+
+      if($('.stock-body-panel').hasClass("open")) {
+        if(-60<=d && d<=0) {
+          $('.stock-body-panel').css({right:  d+60+'px'});
+        } else if (d<-60) {
+          $('.stock-body-panel').css({right:  0+'px'});
+        } else {
+          $('.stock-body-panel').css({right:  60+'px'});
+        }
+      } else {
+        if(0<=d && d<=60) {
+          $('.stock-body-panel').css({right:  d+'px'});
+        } else if(d>60) {
+          $('.stock-body-panel').css({right:  60+'px'});
+        } else {
+          $('.stock-body-panel').css({right:  0+'px'});
+        }
+
+      }
+    }
+  })
+
+  $('.stock-body-panel').on(down, (e)=> {
+    mouseX = e.pageX
+    mousedown = true
+  })
+
+  $("body").on(up, (e)=> {
+    if(mousedown) {
+      if($('.stock-body-panel').hasClass("open")) {
+
+        $('.stock-body-panel').animate({right: '0px'}, 30);
+        $('.stock-body-panel').removeClass("open")
+
+      } else {
+        if (mouseX - e.pageX < 20) {
+          $('.stock-body-panel').animate({right: '0px'}, 30);
+        } else {
+          $('.stock-body-panel').animate({right: '60px'}, 30);
+          $('.stock-body-panel').addClass("open")
+        }
+      }
+      mousedown = false
+    }
+  })
+
+}
+
+tab_event("mousedown", "mousedown", "mouseup")
+tab_event("touchstart", "touchmove", "touchend")
